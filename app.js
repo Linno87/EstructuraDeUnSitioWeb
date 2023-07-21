55c60bf6b10c8b87ee81042451620a8abd62fc0a
Linno87 <linno824@gmail.com>
2023-07-21 04:58:37 -0300

    create:formulario

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const PORT = 3035;
 app.use(express.static("public"));
 
 app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'views', "home.html")); })
-
+app.get('/register', (req, res) => { res.sendFile(path.join(__dirname, 'views', "register.html")); })
+app.get('/login', (req, res) => { res.sendFile(path.join(__dirname, 'views', "login.html")); })
 
 app.listen(PORT, () => {console.log(`Server listening on http://localhost:${PORT}`)})
